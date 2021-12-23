05b8025516d1b89c1408d0e083910625a5c711c6
Aashay Vanpal <arkhamknight.aashay@gmail.com>
2021-12-23 16:20:18 +0530

    deployment test 4

--- server.js
+++ server.js
@@ -63,8 +63,6 @@ app.get('/google/callback',
 
 app.get('/auth/failed', (req, res) => { res.send('something went wrong!') })
 
-app.get('/home',(req,res)=>{ res.redirect('/home')})
-
 app.get('/api/logout', (req, res) => {
     req.logout()
     console.log('logged out backend')
